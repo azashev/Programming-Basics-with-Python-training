d6c2eee16923ddc385e165b518da6637ed36da3b
Alexander Zashev <azashev93@gmail.com>
2022-03-28 17:11:58 +0300

    ~

--- Programming Basics Exam - 2 and 3 May 2019/division_without_reminder.py
+++ Programming Basics Exam - 2 and 3 May 2019/division_without_reminder.py
@@ -9,9 +9,9 @@ for i in range(1, numbers + 1):
     current_number = int(input())
     if current_number % 2 == 0:
         divided_by_2_wr += 1
-    elif current_number % 3 == 0:
+    if current_number % 3 == 0:
         divided_by_3_wr += 1
-    elif current_number % 4 == 0:
+    if current_number % 4 == 0:
         divided_by_4_wr += 1
 
 percent_divided_by_2 = (divided_by_2_wr / numbers) * 100
