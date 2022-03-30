c75faf0d5ff6f2eb9f76966ba0fb3434d7a5a88a
Alexander Zashev <azashev93@gmail.com>
2022-03-31 02:07:51 +0300

    ~

---
+++ Programming Basics Exam - 6 and 7 July 2019/name_game.py
@@ -0,0 +1,36 @@
+import sys
+
+current_winner = ''
+points = 0
+
+#if there are equal records second player wins
+special_points = 0
+special_winner = ''
+condition = False
+
+max_result = -9223372036854775807
+
+while True:
+    player = input()
+    if player == 'Stop':
+        break
+    points = 0
+    for i in range(len(player)):
+        number = int(input())
+        if number == ord(player[i]):
+            points += 10
+        else:
+            points += 2
+
+    if points >= max_result:
+        special_points += 1
+        if special_points == 2:
+            condition = True
+            special_winner = player
+        max_result = points
+        current_winner = player
+
+if condition:
+    print(f"The winner is {special_winner} with {max_result} points!")
+else:
+    print(f"The winner is {current_winner} with {max_result} points!")
