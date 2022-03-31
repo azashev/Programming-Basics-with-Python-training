703da4e1b6cf80a98f527c3c95dc5ec1b160766e
Alexander Zashev <azashev93@gmail.com>
2022-03-31 05:18:29 +0300

    ~

--- Programming Basics Exam - 6 and 7 July 2019/name_game.py
+++ Programming Basics Exam - 6 and 7 July 2019/name_game.py
@@ -1,10 +1,10 @@
 current_winner = ''
 points = 0
 
-
+#if there are multiple winners, second player included, then second player wins
 special_points = 0
 special_winner = ''
-condition = False
+special_result = 0
 
 max_result = 0
 
@@ -22,13 +22,13 @@ while True:
 
     if points >= max_result:
         special_points += 1
-        if special_points == 2:
-            condition = True
-            special_winner = player
         max_result = points
         current_winner = player
+        if special_points == 2:
+            special_winner = player
+            special_result = max_result
 
-if condition:
-    print(f"The winner is {special_winner} with {max_result} points!")
+if special_result >= max_result:
+    print(f"The winner is {special_winner} with {special_result} points!")
 else:
     print(f"The winner is {current_winner} with {max_result} points!")
